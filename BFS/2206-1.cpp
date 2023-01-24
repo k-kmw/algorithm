b2c5758ff9b781aec6f0e79178ed24eb25206209
k-kmw <kimmw2_@naver.com>
2023-01-24 10:07:56 +0900

    1/23

--- BFS/2206-1.cpp
+++ BFS/2206-1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 char board[1001][1001];
-int dist[1001][1001];
+int dist[1001][1001][2];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -14,7 +14,39 @@ int main() {
     cin >> n >> m;
     for(int i=0; i<n; i++)
         for(int j=0; j<m; j++)
+            fill(dist[i][j], dist[i][j]+2, -1);
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
             cin >> board[i][j];
+
+    queue<pair<pair<int, int>, int>> q;
+    dist[0][0][0] = 1;
+    q.push({{0, 0}, 0});
+    while(!q.empty()) {
+        auto cur = q.front();
+        if(cur.first.first == n-1 && cur.first.second == m-1) {
+            cout << dist[n-1][m-1][cur.second];
+            return 0;
+        }
+        q.pop();
+        for(int dir=0; dir<4; dir++) {
+            int nx = cur.first.first + dx[dir];
+            int ny = cur.first.second + dy[dir];
+            int broken = cur.second;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)  continue;
+            if(dist[nx][ny][broken] != -1)  continue;
+            if(board[nx][ny] == '1' && broken != 1 && dist[nx][ny][1] == -1) {
+                dist[nx][ny][1] = dist[cur.first.first][cur.first.second][0] + 1;
+                q.push({{nx, ny}, 1});
+            }
+            if(board[nx][ny] == '0') {
+                dist[nx][ny][broken] = dist[cur.first.first][cur.first.second][broken] + 1;
+                q.push({{nx, ny}, broken});
+            }
+        }
+    }
+    cout << -1;
 
 }
 
---
+++ BFS/9466.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int board[100002];
+bool vis[100002];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int no_team = 0;
+        queue<int> q;
+        for(int i=1; i<=n; i++)
+            cin >> board[i];
+
+        q.push(1);
+        vis[1] = true;
+        int idx = 1;
+        while(!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            int nx = board[cur];
+            if(nx == cur || board[nx] == cur || vis[nx])   {
+                vis[nx] = true;
+                nx = ++idx;
+            }
+            q.push(nx);
+            vis[nx] = true;
+        }
+    }
+
+}
+
+
