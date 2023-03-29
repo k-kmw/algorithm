aa1a033c450eaf390c09a44a44d73eb5e3e42bcc
k-kmw <kimmw2_@naver.com>
2023-03-30 00:23:40 +0900

    3/28

--- 그리디/12865.cpp
+++ 그리디/12865.cpp
@@ -13,7 +13,20 @@ int main(void) {
     for(int i=1; i<=n; i++)
         cin >> w[i] >> v[i];
 
+    int mx = 0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=k; j++) {
+            if(j < w[i]) {
+                d[i][j] = d[i-1][j];
+            }
+            else {
+                d[i][j] = max(d[i-1][j], d[i-1][j-w[i]] + v[i]);
+            }
+            mx = max(mx, d[i][j]);
+        }
+    }
 
+    cout << mx;
 }
 // 그리디로 풀 수 없는데, 잘못 풀 가능성이 있는 문제
 // 무게 대비 가치가 높은 순으로 선택하는 그리디 풀이?
