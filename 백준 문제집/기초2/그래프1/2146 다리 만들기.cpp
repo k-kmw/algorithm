97b47d9f5efc291d3b9794ac38c7dc35ebdce75f
k-kmw <kimmw2_@naver.com>
2024-03-17 10:20:08 +0900

    24/3/16

--- 백준 문제집/기초2/그래프1/2146 다리 만들기.cpp
+++ 백준 문제집/기초2/그래프1/2146 다리 만들기.cpp
@@ -6,7 +6,7 @@ bool vis[105][105];
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, -1, 0, 1};
 int num = 1;
-queue<pair<pair<int, int>, pair<int, int>>> q2; // y, x, 섬의 번호, 거리
+queue<pair<pair<int, int>, int>> q2; // y, x, 섬의 번호, 거리
 
 void bfs(int i, int j) {
     queue<pair<int,int>> q;
@@ -23,7 +23,6 @@ void bfs(int i, int j) {
             vis[ny][nx] = true;
             board[ny][nx] = num;
             q.push({ny, nx});
-            q2.push({{ny, nx}, {board[ny][nx], 0}});
         }
     }
 }
@@ -33,8 +32,8 @@ int main() {
     cin.tie(0);
 
     // 1. bfs 돌면서 섬찾기 - 번호 매기기
-    // 2. 각 섬의 모든 점을 큐에 넣기
-    // 3. 인접이 바다이면 다른 번호의 섬 만날때 까지 계속 탐색
+    // 2. 각 섬의 모든 점에서 최소 거리 섬 찾기
+    // 3. 최소값 갱신하고 다음 점 찾기 -> BFS이므로 가장 가까운 거리가 나와서 더 탐색을 진행할 필요가 없음.
     cin >> n;
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
@@ -46,36 +45,54 @@ int main() {
         for(int j=0; j<n; j++) {
             if(board[i][j] != 0 && !vis[i][j]) {
                 board[i][j] = num;
-                q2.push({{i, j}, {board[i][j], 0}});
                 bfs(i, j);
                 num++;
             }
         }
     }
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cout << board[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+//    for(int i=0; i<n; i++) {
+//        for(int j=0; j<n; j++) {
+//            cout << board[i][j] << ' ';
+//        }
+//        cout << '\n';
+//    }
+
     for(int k=0; k<n; k++)
         fill(vis[k], vis[k]+n, false);
-    while(!q2.empty()) {
-        auto cur = q2.front();
-        q2.pop();
-        cout << cur.first.first << ' ' << cur.first.second << ' ' << cur.second.first << ' ' << cur.second.second << '\n';
-        for(int dir=0; dir<4; dir++) {
-            int ny = cur.first.first + dy[dir];
-            int nx = cur.first.second + dx[dir];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-            if(board[ny][nx] != 0) continue;
-            if(board[ny][nx] != 0 && board[ny][nx] != cur.second.first) {
-                cout << cur.second.second << '\n';
-                return 0;
+
+    int mn = 100*100+1;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(board[i][j] != 0) {
+                q2.push({{i, j}, 0});
+                vis[i][j] = true;
+                while(!q2.empty()) {
+                    auto cur = q2.front();
+//                    cout << cur.first.first << ' ' << cur.first.second << ' ' << cur.second << '\n';
+                    q2.pop();
+                    for(int dir=0; dir<4; dir++) {
+                        int ny = cur.first.first + dy[dir];
+                        int nx = cur.first.second + dx[dir];
+                        if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                        if(vis[ny][nx]) continue;
+                        if(board[ny][nx] != 0 && board[ny][nx] != board[i][j]) {
+                            mn = min(mn, cur.second);
+                            while(!q2.empty()) {
+                                q2.pop();
+                            }
+                            break;
+                        }
+                        if(board[ny][nx] != 0)   continue;
+                        vis[ny][nx] = true;
+                        q2.push({{ny, nx}, cur.second+1});
+                    }
+                }
+                for(int k=0; k<n; k++)
+                    fill(vis[k], vis[k]+n, false);
             }
-            vis[ny][nx] = true;
-            q2.push({{ny, nx}, {cur.second.first,cur.second.second+1}});
         }
     }
+
+    cout << mn << '\n';
 }
