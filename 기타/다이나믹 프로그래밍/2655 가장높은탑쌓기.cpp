735258fe30575cb4597b53511ce98596e42025e7
k-kmw <kimmw2_@naver.com>
2024-04-23 21:08:45 +0900

    24/4/23

--- 기타/다이나믹 프로그래밍/2655 가장높은탑쌓기.cpp
+++ 기타/다이나믹 프로그래밍/2655 가장높은탑쌓기.cpp
@@ -3,82 +3,71 @@ using namespace std;
 
 class Brick {
 public:
-    int area, height, weight;
+    int area, height, weight, idx;
     Brick() = default;
-    Brick(int area, int height, int weight) {
+    Brick(int area, int height, int weight, int idx) {
         this->area = area;
         this->height = height;
         this->weight = weight;
+        this->idx = idx;
     }
-    bool operator < (const Brick& a) {
+    bool operator < (const Brick& a) const {
         return this->area > a.area;
     }
 };
 
-class Trace {
-public:
-    vector<Brick> bricks;
-    int height;
-};
+Brick bricks[101];
+int d[101]; // d[i]: i를 맨 위의 벽돌로 했을 때 높이의 최대값
+int pre[101];
+vector<int> ans;
 
-vector<Brick> bricks;
-Trace d[105][2]; // d[i][0] i번째 상자 담지 않았을 때 높이 최대, d[i][1] i번째 상자 담았을 때 높이 최대
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int n;
     cin >> n;
-    for(int i=0; i<n; i++) {
+    for(int i=1; i<=n; i++) {
         int a, h, w;
         cin >> a >> h >> w;
-        bricks.push_back(Brick(a, h, w));
+        bricks[i] = Brick(a, h, w, i);
     }
-    sort(bricks.begin(), bricks.end());
+    sort(bricks+1, bricks+1+n);
 
-    d[0][1].bricks.push_back(bricks[0]);
-    d[0][1].height = bricks[0].height;
-    d[0][0].height = INT_MAX;
-    //
-    for(int i=1; i<n; i++) {
-
-        // 1. 이전 못쌓고, 현재는 쌓는 경우
-        int sz = d[i-1][0].bricks.size();
-        if(i == 1 && sz == 0) {
-            d[i][1].bricks.push_back(bricks[i]);
-            d[i][1].height = bricks[i].height;
+    int mx = 0;
+    int lastIdx = 0;
+    for(int i=1; i<=n; i++) {
+        int best = bricks[i].height;
+        int bestIdx = 0;
+        for(int j=i-1; j>0; j--) {
+            if(bricks[i].weight < bricks[j].weight) {
+                if(bricks[i].height + d[j] > best) {
+                    bestIdx = j;
+                    best = bricks[i].height + d[j];
+                }
+            }
         }
-        else if(sz > 0 && d[i-1][0].bricks[sz-1].weight > bricks[i].weight) {
-            cout << i << ' ' << d[i][1].height << '\n';
-            d[i][1].bricks.push_back(bricks[i]);
-            d[i][1].height = d[i-1][0].height + bricks[i].height;
-        }
-        // 2. 이전 쌓고, 현재도 쌓을 수 있는 경우
-        sz = d[i-1][1].bricks.size();
-        if(sz > 0 && d[i-1][1].bricks[sz-1].weight > bricks[i].weight) {
-            d[i][1].bricks = vector(d[i-1][1].bricks);
-            d[i][1].bricks.push_back(bricks[i]);
-            d[i][1].height = d[i-1][1].height + bricks[i].height;
-        }
-
-        // 3. 이전 못쌓고, 현재도 못 쌓는 경우
-        sz = d[i-1][0].bricks.size();
-        if(sz > 0 && d[i-1][0].bricks[sz-1].weight < bricks[i].weight) {
-            d[i][0].bricks = vector(d[i-1][0].bricks);
-            d[i][0].bricks = vector(d[i-1][0].bricks);
-            d[i][0].height = d[i-1][0].height;
-        }
-
-        // 4. 이전 쌓고, 현재는 못 쌓는 경우
-        sz = d[i-1][1].bricks.size();
-        if(sz > 0 && d[i-1][1].bricks[sz-1].weight < bricks[i].weight) {
-            d[i][0].bricks = vector(d[i-1][1].bricks);
-            d[i][1].height = d[i-1][1].height;
+        d[i] = best;
+        pre[i] = bestIdx;
+        if(d[i] > mx) {
+            mx = d[i];
+            lastIdx = i;
         }
     }
 
-    cout << d[n-1][1].height << '\n';
-//    for (const auto &item: bricks) {
-//        cout << item.area << '\n';
+    int prior = lastIdx;
+    while(prior != 0) {
+        ans.push_back(bricks[prior].idx);
+        prior = pre[prior];
+    }
+
+    cout << ans.size() << '\n';
+    for(int a : ans) {
+        cout << a << '\n';
+    }
+
+//    for(int i=1; i<=n; i++) {
+//        cout << bricks[i].area << '\n';
 //    }
+    return 0;
 }
