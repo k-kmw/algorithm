0a94d4227c017592eff8dab12b81b4b69ff3fc63
k-kmw <kimmw2_@naver.com>
2024-04-30 22:38:08 +0900

    24/4/30

--- 기타/유니온 파인드/14868 문명.cpp
+++ 기타/유니온 파인드/14868 문명.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n, k;
@@ -11,20 +12,20 @@ int cnt;
 int p[100005];
 
 void make_set() {
-    for(int i=1; i<=k; i++) {
+    for (int i = 1; i <= k; i++) {
         p[i] = i;
     }
 }
 
 int find(int a) {
-    if(a == p[a])   return a;
+    if (a == p[a]) return a;
     return p[a] = find(p[a]);
 }
 
 void merge(int a, int b) {
     int rootA = find(a);
     int rootB = find(b);
-    if(rootA == rootB)  return;
+    if (rootA == rootB) return;
 //    cout << "union: " << a << ' ' << b << '\n';
     p[rootA] = rootB;
     cnt++;
@@ -41,9 +42,10 @@ int main() {
     // 4. 모든 문명이 결합되었는지 확인 - union할 때마다 cnt를 증가시켜서 k와 같은지 비교
     // 5. 모든 문명이 결합되지 않았으면 3번부터 반복
     queue<pair<int, int>> q;
-    for(int i=0; i<k; i++) {
+    queue<pair<int, int>> q2;
+    for (int i = 0; i < k; i++) {
         int x, y;
-        cin >> y >> x;
+        cin >> x >> y;
         board[y][x] = num++;
         q.push({y, x});
         vis[y][x] = true;
@@ -52,41 +54,56 @@ int main() {
     make_set();
 
     int year = 0;
-    while(true) {
-        queue<pair<int, int>> q2;
-        year++;
+    while (!q.empty()) {
+
+//        cout << cur.second << '\n';
+//        cout << cur.first << ' ' << cur.second << ' ' << board[cur.first][cur.second] << '\n';
+
+        // 인접한 문명 확인하고 결합
         while(!q.empty()) {
             auto cur = q.front();
             q.pop();
-//            cout << cur.first << ' ' << cur.second << ' ' << board[cur.first][cur.second] << '\n';
+            q2.push(cur);
+            int y = cur.first;
+            int x = cur.second;
+
+            for(int dir=0; dir<4; dir++) {
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                if(ny < 1 || ny > n || nx < 1 || nx > n) continue;
+                if(board[ny][nx] == board[y][x] || board[ny][nx] == 0)  continue;
+                merge(board[ny][nx], board[y][x]);
+            }
+        }
+
+//        cout << year << '\n';
+//        for(int i=n; i>=1; i--) {
+//            for(int j=1; j<=n; j++) {
+//                cout << board[i][j] << ' ';
+//            }
+//            cout << '\n';
+//        }
+//        cout << '\n';
+
+        if(cnt == k-1) {
+            cout << year << '\n';
+            return 0;
+        }
+
+        while(!q2.empty()) {
+            auto cur = q2.front();
+            q2.pop();
+            // 문명 전파
             for(int dir=0; dir<4; dir++) {
                 int ny = cur.first + dy[dir];
                 int nx = cur.second + dx[dir];
                 if(ny < 1 || ny > n || nx < 1 || nx > n) continue;
-                if(vis[ny][nx]) continue;
+                if(board[ny][nx] != 0 || vis[ny][nx]) continue;
                 board[ny][nx] = board[cur.first][cur.second];
-                for(int d=0; d<4; d++) {
-                    int nny = ny + dy[d];
-                    int nnx = nx + dx[d];
-                    if(nny < 1 || nny > n || nnx < 1 || nnx > n) continue;
-                    if(board[nny][nnx] == board[ny][nx] || board[nny][nnx] == 0) continue;
-                    merge(board[nny][nnx], board[ny][nx]);
-                }
-                q2.push({ny, nx});
+                q.push({ny, nx});
                 vis[ny][nx] = true;
-
-                if(cnt == k-1) {
-                    cout << year << '\n';
-                    return 0;
-                }
             }
         }
-        while(!q2.empty()) {
-            auto tmp = q2.front();
-            q2.pop();
-            q.push(tmp);
-        }
+        year++;
     }
 }
-
-// 못품 내일 다시..
