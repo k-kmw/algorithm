51790377dbd785b77e090e5d9477ae0e82b9da91
k-kmw <kimmw2_@naver.com>
2024-05-11 23:58:12 +0900

    24/5/11

--- 백준 문제집/중급1/브루트포스/16197 두 동전.cpp
+++ 백준 문제집/중급1/브루트포스/16197 두 동전.cpp
@@ -1,62 +1,52 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int n, m;
 char board[25][25];
-vector<pair<int, int>> coins;
-int ans;
+bool vis[2][25][25];
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, -1, 0, 1};
-bool vis[2][25][25];
-bool isOut[2];
+vector<pair<int, int>> coins;
+int ans = INT_MAX;
 
-// k번째 버튼을 dir방향으로 누른다.
-void dfs(int k, int c, int o) {
-    if (k == 11) {
+bool isOut(int y, int x) {
+    if(y < 0 || y >= n || x < 0 || x >= m)  return true;
+    return false;
+}
+
+void dfs(int k, int y, int x, int yy, int xx) {
+    if(k >= 11) {
         return;
     }
-    if (ans != 0) {
-        return;
-    }
-    if (c == 1) {
-        int cnt = 0;
-        for (auto coin: coins) {
-            int y = coin.first;
-            int x = coin.second;
-            if (y < 0 || y >= n || x < 0 || x >= m) cnt++;
+    for(int dir=0; dir<4; dir++) {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        int nyy = yy + dy[dir];
+        int nxx = xx + dx[dir];
+
+//        cout << ny << ' ' << nx << '\n';
+//        cout << nyy << ' ' << nxx << '\n';
+
+        if(isOut(ny, nx) && isOut(nyy, nxx)) {
+            continue;
         }
-        if (cnt == 1) {
-            ans = k;
+        if(isOut(ny, nx) && !isOut(nyy, nxx)) {
+            ans = min(ans, k);
             return;
         }
-    }
-
-    for (int dir = 0; dir < 4; dir++) {
-        int y = coins[c].first;
-        int x = coins[c].second;
-        cout << y << ' ' << x << '\n';
-        y += dy[dir];
-        x += dx[dir];
-        if (c >= 0 && c < 2 && y >= 0 && y < n && x >= 0) {
-            if(isOut[c]) {
-                dfs(k, (c%2)+1, o);
-            }
-            if(vis[c][y][x]) continue;
-            coins[c].first = y;
-            coins[c].second = x;
-            vis[c][y][x] = true;
-
-            dfs(k+1, (c%2)+1, o);
-
-            vis[c][y][x] = false;
-            coins[c].first -= dy[dir];
-            coins[c].second -= dx[dir];
-        } else {
-            isOut[c] = true;
-            dfs(k+1, (c%2)+1, o+1);
-            isOut[c] = false;
+        if(!isOut(ny, nx) && isOut(nyy, nxx)) {
+            ans = min(ans, k);
+            return;
         }
+        if(board[ny][nx] == '#') {
+            ny = y;
+            nx = x;
+        }
+        if(board[nyy][nxx] == '#') {
+            nyy = yy;
+            nxx = xx;
+        }
+        dfs(k+1, ny, nx, nyy, nxx);
     }
 }
 
@@ -64,22 +54,19 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 1. 동전 2개를 이동 시킴 상/하/좌/우 (백트래킹)
-    // 2. 하나의 동전만 떨어진 상태인지 확인
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
             cin >> board[i][j];
-            if (board[i][j] == 'o') {
+            if(board[i][j] == 'o') {
                 coins.push_back({i, j});
             }
         }
     }
-    for (int i = 0; i < 2; i++) {
-        int y = coins[i].first;
-        int x = coins[i].second;
-        vis[i][y][x] = true;
+    dfs(1, coins[0].first, coins[0].second, coins[1].first, coins[1].second);
+    if(ans == INT_MAX) {
+        cout << -1 << '\n';
+    } else {
+        cout << ans << '\n';
     }
-    dfs(0, 0, 0);
-    cout << ans << '\n';
 }
